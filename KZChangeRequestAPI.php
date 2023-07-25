9aaf85896804cf801d8037809a4b6923418db9be
Joel Rothschild <joel.rothschild@jacksonriver.com>
2023-07-25 12:27:11 +0300

    Removed API class. Sticking with server-side form processing for now.

--- KZChangeRequestAPI.php
+++
@@ -1,9 +0,0 @@
-<?php
-class KZChangeRequestAPI extends ApiBase
-{
-  public function execute()
-  {
-    $this->getResult()->addValue(null, $this->getModuleName(), "Hello World");
-    //wfDebug(print_r($this->getResult(),true));
-  }
-}
