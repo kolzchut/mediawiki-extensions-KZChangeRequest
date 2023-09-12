fcfed8d0ff1fa6145afe1e6913d0c2d95b7f1c7b
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2023-09-12 12:39:33 +0300

    Fix the Hebrew name of the special page

--- KZChangeRequest.i18n.alias.php
+++ KZChangeRequest.i18n.alias.php
@@ -2,21 +2,11 @@
 
 $specialPageAliases = [];
 
-/*
-$specialPageAliases['en'] = [
-  'KZChangeRequest' => ['KZChangeRequest', 'Kol-Zchut Change Request Form'],
-];
-
-$specialPageAliases['he'] = [
-  'KZChangeRequest' => ['בקשת_שינוי', 'טופס בקשת שינוי לאתר כל-זכות'],
-];
-*/
-
 /** English (English) */
 $specialPageAliases['en'] = [
-  'KZChangeRequest' => ['KZChangeRequest', 'Change Request'],
+  'KZChangeRequest' => ['KZChangeRequest', 'Change Request']
 ];
 
 $specialPageAliases['he'] = [
-  'KZChangeRequest' => ['בקשת_שינוי'],
+  'KZChangeRequest' => ['הצעת_שינוי']
 ];
